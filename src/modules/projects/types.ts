128d53f2bae7a85f8036f619c9130d6fae69bae5
Humberto <humberto.bernal@utec.edu.pe>
2022-12-19 08:40:41 -0500

    feat: add user and project types

---
+++ src/modules/projects/types.ts
@@ -0,0 +1,49 @@
+import { User } from "../auth/types"
+
+
+export enum LogStatus {
+    TODO = 1,
+    IN_PROGRESS = 2,
+    COMPLETED = 3,
+    IN_REVIEW = 4,
+    ACCEPTED = 5,
+    REJECTED = 6,
+    DONE = 7,
+  }
+
+export enum Priority {
+    NONE = 0,
+    LOW = 1,
+    NORMAL = 2,
+    HIGH = 3,
+    URGENT = 4,
+  }
+
+  export type RelevantPoint = {
+    name: string,
+    descriptors: string[],
+  }
+
+  export type Log = {
+    id: string,
+    project_id: string,
+    name: string,
+    relevant_points: RelevantPoint[],
+    status: LogStatus,
+    priority: Priority,
+    
+    created_at: string,
+    updated_at: string
+    created_by: User,
+}
+
+export type Project = {
+    id: string,
+    name: string,
+    description: string,
+    logs: Log[]
+    
+    created_at: string,
+    updated_at: string,
+    created_by: User,
+}
