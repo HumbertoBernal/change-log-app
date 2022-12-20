3004457da5d11e7913daf7937c7d65dae805505d
Humberto <humberto.bernal@utec.edu.pe>
2022-12-19 19:04:43 -0500

    refactor: add general crud function to make it scalable

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,5 +1,6 @@
-import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
-import { CreateProject, Project, UpdateProject } from "@/modules/projects/types";
+import { Filter, MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import { CreateProject, FilterProject, PatchProject, Project, ProjectQuery } from "@/modules/projects/types";
+import { getQuery } from "@/modules/projects/utils";
 
 
 // Connection URL
@@ -7,92 +8,95 @@ const client = new MongoClient(process.env.MONGO_URI ?? '', { serverApi: ServerA
 
 // Database Name
 const dbName = 'changeLogDB';
-const collectionName = 'documents';
+const projectCollectionName = 'projects';
 
-const createProject = async (name: string, description: string) => {
+const create = async <TData>(info: TData, dbName: string, collectionName: string) => {
 
     await client.connect();
 
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    const now = new Date().toUTCString();
+    const now = new Date().toISOString();
 
-    const project: CreateProject = {
-        name: name,
-        description: description,
+    const data = {
+        ...info,
         created_at: now,
         updated_at: now,
-        created_by: { name: "John Doe", email: "jdow@gmail.com" }
+        created_by: { name: "Michael Doe", email: "jdowmich@gmail.com" }
     }
 
-    const result = await collection.insertOne(project);
+    const result = await collection.insertOne(data);
 
     client.close();
 
     return result.acknowledged ? result.insertedId : null
 }
 
-const getProject = async (id: string) => {
+const get = async <TData>(id: string, dbName: string, collectionName: string) => {
     await client.connect();
 
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    const findResult = (await collection.findOne({ "_id": new ObjectId(id) })) as unknown as Project;
-
-
-    console.log(findResult);
-    client.close()
-
-    return findResult
-}
-
-const listProjects = async () => {
-    await client.connect();
-
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-    const findResult = (await collection.find({}).toArray()) as unknown as Project[];
-
-    console.log(findResult);
+    const findResult = (await collection.findOne({ "_id": new ObjectId(id) })) as unknown as TData;
 
     client.close()
 
     return findResult
 }
 
-const updateProjects = async (id: string, project: UpdateProject) => {
+const list = async <TData>(query: Filter<Document> , dbName: string, collectionName: string) => {
     await client.connect();
-
 
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    const updateResult = await collection.updateOne({ "_id": new ObjectId(id) }, { $set: project });
+    const findResult = (await collection.find(query).toArray()) as unknown as TData[];
+    client.close()
+    return findResult
+};
+
+const update = async <TData>(id: string, project: TData, dbName: string, collectionName: string) => {
+    await client.connect();
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    const updatedProject = { ...project, updated_at: new Date().toISOString() }
+    const updateResult = await collection.updateOne({ "_id": new ObjectId(id) }, { $set: updatedProject });
 
     client.close();
 
     return updateResult.acknowledged ? id : null
 }
 
-const deleteProject = async (id: string) => {
+const remove = async (id: string, dbName: string, collectionName: string) => {
     await client.connect();
 
     const db = client.db(dbName);
-    const collection = db.collection('documents');
+    const collection = db.collection(collectionName);
 
     const deleteResult = await collection.deleteOne({ "_id": new ObjectId(id) });
 
     client.close();
 
-    return deleteResult.acknowledged ? id : null
+    return deleteResult
 }
+
+const createProject = (name: string, description: string) => create<CreateProject>({ name, description }, dbName, projectCollectionName);
+const getProject = (id: string) => get<Project>(id, dbName, projectCollectionName);
+const listProjects = async (filters?: FilterProject) => list<Project>(getQuery(filters), dbName, projectCollectionName);
+const patchProject = async (id: string, project: PatchProject) => update<PatchProject>(id, project, dbName, projectCollectionName);
+const putProject = async (id: string, project: Project) => update<Project>(id, project, dbName, projectCollectionName);
+const deleteProject = async (id: string) => remove(id, dbName, projectCollectionName);
+
 
 export const MongoService = {
     createProject,
     getProject,
     listProjects,
-    updateProjects,
+    patchProject,
+    putProject,
     deleteProject,
 }
