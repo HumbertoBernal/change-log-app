a58da94eaed09bc56df84aa219919d9628fbdb11
Humberto <humberto.bernal@utec.edu.pe>
2022-12-21 10:15:45 -0500

    feat: add landing page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Change Log App is a simple app that allows you to keep track of changes in your project."
+        ></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/modules/landing/HeroSection.tsx
@@ -0,0 +1,48 @@
+
+import Link from 'next/link'
+
+
+export default function HeroSection() {
+  return (
+    <div className="relative">
+      <div className="absolute inset-x-0 bottom-0 bg-gray-100" />
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+          <div className="absolute inset-0">
+            <img
+              className="h-full w-full object-cover"
+              src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+              alt="People working on laptops"
+            />
+            <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-blue-800 mix-blend-multiply" />
+          </div>
+          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+            <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+              <span className="block text-white">Streamline Your  </span>
+              <span className="block text-indigo-200">Change Management</span>
+            </h1>
+            <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
+              Manage and track changes to your projects and products easily with CLApp. Our platform streamlines your change management process for improved efficiency.
+            </p>
+            <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+              <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                <Link
+                  href="api/auth/login"
+                  className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-blue-700 bg-white hover:bg-indigo-50 sm:px-8"
+                >
+                  Log In
+                </Link>
+                <Link
+                  href="api/auth/signin"
+                  className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 bg-opacity-60 hover:bg-opacity-80 sm:px-8"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
