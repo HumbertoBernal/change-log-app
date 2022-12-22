dd54bfc85616ef90627f42363a5539fd2b8dcdee
Humberto <humberto.bernal@utec.edu.pe>
2022-12-21 20:28:35 -0500

    fix: add color to loading spinner

--- src/common/components/LoadingSpinner.tsx
+++ src/common/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ const LoadingSpiner = () => {
     return (
       <svg
         role="status"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-primary"
+        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-700"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
